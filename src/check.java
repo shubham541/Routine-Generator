e8e2900645c7f5e6e1f416411bc37c0e19c7fd2e
kumar Shubham <37610720+shubham541@users.noreply.github.com>
2020-07-01 10:07:34 +0530

    Add files via upload

---
+++ src/check.java
@@ -0,0 +1,37 @@
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+
+
+/**
+ *
+ * @author kunal
+ */
+public class check {
+    public static void main(String[] args) throws IOException{
+        
+        String st,st1="";
+ 
+        File file = new File("/home/kunal/workplace/netbeans-project/Scheduler/src/res/path.txt");
+        FileWriter wr=new FileWriter("/home/kunal/workplace/netbeans-project/Scheduler/src/res/path.txt");
+
+        
+        BufferedReader br = new BufferedReader(new FileReader(file));
+
+        
+        while ((st = br.readLine()) != null){
+                  
+                st1=st1+st;}
+        
+        
+        System.out.println(st1);
+        String st2="kunal";
+        wr.write(st2);
+        
+    }
+    
+}
